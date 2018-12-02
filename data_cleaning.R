67465ad64d8f1cea249ea0e98fd5d1f8bb3f852b
Zach Wu <zachwu23@uw.edu>
2018-12-02 10:59:56 -0800

    dataset for question #1

--- data_cleaning.R
+++ data_cleaning.R
@@ -2,13 +2,18 @@
 
 library(readr)
 library(dplyr)
-raw_data <- read_csv("/info201_project/0_data/MERGED2009_10_PP.csv")
+raw_data <- read_csv("script_costs.csv")
 
 # This will get the costs associated with the school while keeping the school info
-costs <- raw.data %>%
-  select(UNITID,	OPEID,	OPEID6,	INSTNM,	CITY,	STABBR,	ZIP,
-         "INSTATE TUITION" = TUITIONFEE_IN,
-         "OUTOFSTATE TUITION" = TUITIONFEE_OUT)
-costs[is.na(costs)] <- 0
+costs <- raw_data %>%
+  select("ID" = UNITID,
+         "NAME" = INSTNM,
+         CITY, 
+         "STATE" = STABBR,
+         ZIP,
+         "INSTATE_TUITION" = TUITIONFEE_IN,
+         "OUTOFSTATE_TUITION" = TUITIONFEE_OUT) %>%
+  filter(INSTATE_TUITION != "NULL" & OUTOFSTATE_TUITION != "NULL") %>%
+  mutate(ZIP = substr(ZIP, 1, 5))
 
-write.csv(costs, "uni_costs.csv")
+write.csv(costs, "uni_costs_by_location.csv", row.names = FALSE)

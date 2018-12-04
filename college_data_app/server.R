46ed54eb9b4f2f8b442ed648b17d8449b07409c0
Hawk Ticehurst <hawkticehurst@gmail.com>
2018-12-03 18:29:33 -0800

    Add CSS file and changes to Shiny UI

--- college_data_app/server.R
+++ college_data_app/server.R
@@ -6,7 +6,28 @@ library(shiny)
 
 shinyServer(function(input, output) {
    
-  output$distPlot <- renderPlot({
+  output$distPlot1 <- renderPlot({
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+  
+  output$distPlot2 <- renderPlot({
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+  
+  output$distPlot3 <- renderPlot({
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+  
+  output$distPlot4 <- renderPlot({
     x    <- faithful[, 2] 
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
--- college_data_app/ui.R
+++ college_data_app/ui.R
@@ -4,22 +4,113 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
-  titlePanel("College Data"),
-  
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    mainPanel(
-       plotOutput("distPlot")
-    )
+  tags$head(
+    tags$title("College Data Webapp"),
+    tags$link(rel="stylesheet", type = "text/css", href="index.css"),
+    tags$meta(charset="utf-8"),
+    tags$meta(name="viewport", content="width=device-width, initial-scale=1, shrink-to-fit=no")
+  ),
+
+  navbarPage(id = "navbar",
+             collapsible = TRUE,
+             header = TRUE,
+             position = c("fixed-top"),
+             title = "College Data Webapp",
+             
+             ## Project Page Content
+             tabPanel("Project", 
+                      tags$div(class = "jumbotron",
+                               h1(class = "focus-in-contract",
+                               "College Data"),
+                               p("An information resource for incoming college students.")
+                      ),
+                      
+                      ## Research Question 1 Visualization
+                      tags$div(class = "viz-card",
+                               h3("Cost of College By Location"),
+                               p("What are the costs of college by zip code? How about if you're an in-state vs. out-of-state student?"),
+                               sidebarLayout(
+                                 sidebarPanel(
+                                   sliderInput("bins",
+                                               "Number of bins:",
+                                               min = 1,
+                                               max = 50,
+                                               value = 30)
+                                 ),
+                                 
+                                 mainPanel(
+                                   plotOutput("distPlot1")
+                                 )
+                               )         
+                      ),
+                      
+                      ## Research Question 2 Visualization
+                      tags$div(class = "viz-card",
+                               h3("Highest Post-College Earnings"),
+                               p("What colleges have the highest post-college average earnings depending on Public, Private Nonprofit, and Private For-Profit colleges?"),
+                               sidebarLayout(
+                                 sidebarPanel(
+                                   sliderInput("bins",
+                                               "Number of bins:",
+                                               min = 1,
+                                               max = 50,
+                                               value = 30)
+                                 ),
+                                 
+                                 mainPanel(
+                                   plotOutput("distPlot2")
+                                 )
+                               )        
+                      ),
+                      
+                      ## Research Question 3 Visualization
+                      tags$div(class = "viz-card",
+                               h3("Best Repayment Rates"),
+                               p("What colleges have the best repayment rate (for 1, 3, 5, and 7 years) based on family income?"),
+                               sidebarLayout(
+                                 sidebarPanel(
+                                   sliderInput("bins",
+                                               "Number of bins:",
+                                               min = 1,
+                                               max = 50,
+                                               value = 30)
+                                 ),
+                                 
+                                 mainPanel(
+                                   plotOutput("distPlot3")
+                                 )
+                               )      
+                      ),
+                      
+                      ## Research Question 4 Visualization
+                      tags$div(class = "viz-card",
+                               h3("Largest Debt"),
+                               p("Which schools have the greatest cumulative median debt disaggregated by student subgroups?"),
+                               sidebarLayout(
+                                 sidebarPanel(
+                                   sliderInput("bins",
+                                               "Number of bins:",
+                                               min = 1,
+                                               max = 50,
+                                               value = 30)
+                                 ),
+                                 
+                                 mainPanel(
+                                   plotOutput("distPlot4")
+                                 )
+                               )       
+                      )
+             ),
+             
+             ## About Page Content
+             tabPanel("About", 
+                      "contents2"
+             ),
+             
+             ## Team Page Content
+             tabPanel("Team", 
+                      "contents3"
+             )
   )
 ))

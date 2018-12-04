d3cfc3150413c38fdb2f8e942c132b2c02b057e0
Hawk Ticehurst <hawkticehurst@gmail.com>
2018-12-04 01:00:01 -0800

    Add more styling to Shiny UI.

--- college_data_app/ui.R
+++ college_data_app/ui.R
@@ -32,15 +32,18 @@ shinyUI(fluidPage(
                         tags$div(class = "wave wave3")
                       ),
                       
-                      h3(id = "color-bar1", "Cost of College"),
-                      p(class = "summary-text",
-                        "It's no secret that college is expensive. It's more important than 
-                        ever to perform due diligence when exploring your collegiate options. 
-                        What are the costs of college by zip code? How about if you're an 
-                        in-state vs. out-of-state student?"),
+                      ## Summary of Research Question 1
+                      tags$div(class = "summary-content-container",
+                               h3(id = "color-bar1", class = "summary-title", "Cost of College"),
+                               p(class = "summary-text",
+                                 "It's no secret that college is expensive. It's more important 
+                                 than ever to perform due diligence when exploring your 
+                                 collegiate options. What are the costs of college by zip code? 
+                                 How about if you're an in-state vs. out-of-state student?")     
+                      ),
                       
                       ## Research Question 1 Visualization
-                      tags$div(class = "viz-card",
+                      tags$div(class = "viz-container",
                                sidebarLayout(
                                  sidebarPanel(
                                    selectInput("stateStatus",
@@ -56,13 +59,16 @@ shinyUI(fluidPage(
                                )         
                       ),
                       
-                      h3(id = "color-bar1", "Post-College Earnings"),
-                      p(class = "summary-text",
-                        "What colleges have the highest post-college average earnings depending
-                        on Public, Private Nonprofit, and Private For-Profit colleges?"),
+                      ## Summary of Research Question 2
+                      tags$div(class = "summary-content-container", 
+                               h3(id = "color-bar2", class = "summary-title", "Post-College Earnings"),
+                               p(class = "summary-text",
+                                 "What colleges have the highest post-college average earnings depending
+                                 on Public, Private Nonprofit, and Private For-Profit colleges?")     
+                      ),
                       
                       ## Research Question 2 Visualization
-                      tags$div(class = "viz-card",
+                      tags$div(class = "viz-container",
                                sidebarLayout(
                                  sidebarPanel(
                                    selectInput("typeOfCollege",
@@ -76,13 +82,16 @@ shinyUI(fluidPage(
                                )        
                       ),
                       
-                      h3(id = "color-bar1", "Best Repayment Rates"),
-                      p(class = "summary-text", 
-                        "What colleges have the best repayment rate (for 1, 3, 5, and 7 years) 
-                        based on family income?"),
+                      ## Summary of Research Question 3
+                      tags$div(class = "summary-content-container", 
+                               h3(id = "color-bar3", class = "summary-title", "Best Repayment Rates"),
+                               p(class = "summary-text", 
+                                 "What colleges have the best repayment rate (for 1, 3, 5, and 7 years) 
+                                 based on family income?")
+                      ),
                       
                       ## Research Question 3 Visualization
-                      tags$div(class = "viz-card",
+                      tags$div(class = "viz-container",
                                sidebarLayout(
                                  sidebarPanel(
                                    selectInput("repaymentYears",
@@ -96,13 +105,16 @@ shinyUI(fluidPage(
                                )      
                       ),
                       
-                      h3(id = "color-bar1", "Largest Debt"),
-                      p(class = "summary-text", 
-                        "Which schools have the greatest cumulative median debt disaggregated 
-                        by student subgroups?"),
+                      ## Summary of Research Question 4
+                      tags$div(class = "summary-content-container", 
+                               h3(id = "color-bar4", class = "summary-title", "Largest Debt"),
+                               p(class = "summary-text", 
+                                 "Which schools have the greatest cumulative median debt disaggregated 
+                                 by student subgroups?")
+                      ),
                       
                       ## Research Question 4 Visualization
-                      tags$div(class = "viz-card",
+                      tags$div(class = "viz-container",
                                sidebarLayout(
                                  sidebarPanel(
                                    selectInput("studentSubgroup",
